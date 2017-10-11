f34070f9e911e6dbd6de31d4febcf1b565a742a4
scsujith <scsujith@gmail.com>
2017-10-11 11:36:07 +0530

    Add files via upload

---
+++ src/components/search_bar_oldv1.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+//const SearchBar = () => {
+//  return(
+//     <div> <input />
+//     <h3>hi</h3>
+//     </div>
+//   );
+// };
+class SearchBar extends Component {
+  render() {
+      return <input onchange={this.onInputChange} />;
+  }
+  onInputChange(event){
+    console.log(event.target.value);
+  }
+}
+export default SearchBar;
